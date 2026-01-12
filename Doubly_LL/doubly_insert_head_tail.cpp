d0c60218d6d91b2e240048789de9cf1ed1fd1a06
Shihab-004 <shihabul.islam.shihab.004@gmail.com>
2026-01-13 00:52:02 +0600

    insert head and tail easy, fan of doubly

---
+++ Doubly_LL/doubly_insert_head_tail.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+
+};
+
+void insert_head(Node* &head,int val){
+    Node* newNode = new Node(val);
+    newNode->next=head;
+    head->prev=newNode;
+    head=newNode;
+
+}
+
+void insert_tail(Node* &tail,int val){
+    Node* newNode = new Node(val);
+    newNode->prev=tail;
+    tail->next=newNode;
+    tail=newNode;
+}
+void  print_forward(Node* head){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+}
+
+
+int main() {
+    Node *head=new Node(990);
+    Node *a=new Node(770);
+    Node *tail=new Node(404);
+
+    head->next=a;
+    a->prev=head;
+    a->next=tail;
+    tail->prev=a;
+    
+    insert_head(head,333);
+    insert_tail(tail,6969);
+    print_forward(head);
+   
+    
+    
+    return 0;
+}
